0a49c9438a6cf37c3ed3468804929dc7ce6f8836
rhijjawi <18570300+rhijjawi@users.noreply.github.com>
2022-02-24 20:01:45 +0300

    Update find-replace-wallets.js

--- find-replace-wallets.js
+++ find-replace-wallets.js
@@ -1,6 +1,6 @@
 let list = [1,3,5,7]
 wallet = {"BTC":"bc1qkt7vsxzlv6gh80w6hdt790w952qgs0c76qrmkq","LTC":"LX5NJLSE3gNrmgJurmEA8tYzMopaWJUGyB","XMR":"496n1d89oXpdfjzR8GjuJQLc889enMxuMMZwVbFrKpoNRcoqLwThXJeYG91YYCz9YoPpmaf4TL1B4eZu7GMPxAGVL6RrxvP", "ETH":"0xd85167068eBB5287e4753646E08118284e270565"}
-window.addEventListener('load', function () {
+document.addEventListener("DOMContentLoaded", function(event) {
     let cryp = document.querySelector('footer').querySelector('p').querySelectorAll('a')
     for (i of list) {
         console.log()
