03f05bf319f201faa98196c80b7c67f8f5ca3e8f
rhijjawi <18570300+rhijjawi@users.noreply.github.com>
2022-02-25 15:02:19 +0300

    Update torrents.js

--- torrents.js
+++ torrents.js
@@ -1,4 +1,7 @@
 torrents = document.getElementById('torrents')
 for (i of Array(document.getElementById("torrents").getElementsByTagName("li").length).keys()) {
-    console.log(i)
+    let mag = document.getElementById("torrents").getElementsByTagName("li")[i].getElementsByClassName('item-icons')[0].getElementsByTagName('a')[0]
+    mag = mag.getAttribute('href');
+    console.log(mag)
+    //console.log(document.getElementById("torrents").getElementsByTagName("li")[i])
 }
