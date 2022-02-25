791bf94e179e7769e33f7a9a5ab0c0cd167d2532
rhijjawi <18570300+rhijjawi@users.noreply.github.com>
2022-02-26 01:33:02 +0300

    Update torrents.js

--- torrents.js
+++ torrents.js
@@ -1,7 +1,17 @@
 torrents = document.getElementById('torrents')
 for (i of Array((document.getElementById("torrents").getElementsByTagName("li").length)-1).keys()) {
+    let nm = ''
     let mag = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].querySelectorAll('a')[0];
+    let name = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-name')[0].querySelectorAll('a')[0].innerHTML;
     mag = mag.href;
-    console.log(mag)
-    //console.log(document.getElementById("torrents").getElementsByTagName("li")[i])
+    let urlenc = "https://api.ramzihijjawi.me/add-torrent?magnet="+mag
+    let source = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].getElementsByTagName('img')[1];
+    if (typeof(source) == 'object') {
+        let source = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].getElementsByTagName('img')[1].getAttribute('alt');
+        if (source == "VIP" || source == "Trusted") {
+            urlenc = urlenc + '&source=' + source;
+        }
+        else {}
+    }
+    else {urlenc = urlenc + '&source=untrusted';}
 }
