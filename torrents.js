806b434701b65914c9255e14c966c0a41bb8f007
rhijjawi <18570300+rhijjawi@users.noreply.github.com>
2022-02-26 01:46:37 +0300

    Update torrents.js

--- torrents.js
+++ torrents.js
@@ -1,18 +1,20 @@
-torrents = document.getElementById('torrents')
-for (i of Array((document.getElementById("torrents").getElementsByTagName("li").length)-1).keys()) {
-    let nm = ''
-    let mag = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].querySelectorAll('a')[0];
-    let name = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-name')[0].querySelectorAll('a')[0].innerHTML;
-    mag = mag.href;
-    let urlenc = "https://api.ramzihijjawi.me/add-torrent?magnet="+mag
-    let source = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].getElementsByTagName('img')[1];
-    if (typeof(source) == 'object') {
-        let source = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].getElementsByTagName('img')[1].getAttribute('alt');
-        if (source == "VIP" || source == "Trusted") {
-            urlenc = urlenc + '&source=' + source;
+document.addEventListener("DOMContentLoaded", function(event) {
+    torrents = document.getElementById('torrents')
+    for (i of Array((document.getElementById("torrents").getElementsByTagName("li").length)-1).keys()) {
+        let nm = ''
+        let mag = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].querySelectorAll('a')[0];
+        let name = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-name')[0].querySelectorAll('a')[0].innerHTML;
+        mag = mag.href;
+        let urlenc = "https://api.ramzihijjawi.me/add-torrent?magnet="+mag
+        let source = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].getElementsByTagName('img')[1];
+        if (typeof(source) == 'object') {
+            let source = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].getElementsByTagName('img')[1].getAttribute('alt');
+            if (source == "VIP" || source == "Trusted") {
+                urlenc = urlenc + '&source=' + source;
+            }
+            else {}
         }
-        else {}
+        else {urlenc = urlenc + '&source=untrusted';}
+        document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].querySelectorAll('a')[0].href = urlenc;
     }
-    else {urlenc = urlenc + '&source=untrusted';}
-    document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].querySelectorAll('a')[0].href = urlenc;
-}
+});
