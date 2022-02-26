e242b81567a6c146db53064fee8ce9b6228ecacc
rhijjawi <18570300+rhijjawi@users.noreply.github.com>
2022-02-26 18:01:29 +0300

    Update form.js

--- form.js
+++ form.js
@@ -6,7 +6,6 @@ function subForm(){
     tor = document.getElementById('t').value
     req = {"magnet": tor, "name": torname}
     const res = axios.post('/torrent/add-magnet', req, {headers: {'Authorization' : document.getElementById('key').value}
-    });
-    console.log(res.data)
+    }).then((response) => {Swal.fire({icon: 'success',title: 'That worked!', text: 'This torrent has been added to your account!',footer: '<p>Click <a href="https://api.ramzihijjawi.me/torrent/get-status">here</a> to check your torrent status</p>'})}, (error) => {Swal.fire({icon: 'error',title: 'Oops...',text: "The API key you've inserted is invalid",footer: '<a href="">Why do I have this issue?</a>'})});
     return false;
 }
