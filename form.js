1adda38bcb77895a4997128ffe86557cba2d55f3
rhijjawi <18570300+rhijjawi@users.noreply.github.com>
2022-02-27 00:14:18 +0300

    Update form.js

--- form.js
+++ form.js
@@ -2,10 +2,11 @@ document.addEventListener("DOMContentLoaded", function(event) {
   document.getElementsByTagName('form')[0].addEventListener("submit", {subForm});
 });
 function subForm(){
-    torname = document.getElementById('tname').value
-    tor = document.getElementById('t').value
-    req = {"magnet": tor, "name": torname}
-    const res = axios.post('/torrent/add-magnet', req, {headers: {'Authorization' : document.getElementById('key').value}
-    }).then((response) => {Swal.fire({icon: 'success',title: 'That worked!', text: 'This torrent has been added to your account!',footer: '<p>Click <a href="https://api.ramzihijjawi.me/torrent/get-status">here</a> to check your torrent status</p>'})}, (error) => {Swal.fire({icon: 'error',title: 'Oops...',text: "The API key you've inserted is invalid",footer: '<a href="">Why do I have this issue?</a>'})});
+    let torname = document.getElementById('tname').value;
+    let tor = document.getElementById('t').value;
+    let key = document.getElementById('key').value;
+    let req = {"magnet": tor, "name": torname};
+    const res = axios.post('/torrent/add-magnet', req, {headers: {'Authorization' : key};
+    }).then((response) => {Swal.fire({icon: 'success',title: 'That worked!', text: 'This torrent has been added to your account!',footer: `<p>Click <a href="https://api.ramzihijjawi.me/torrent/get-status?apikey=${key}">here</a> to check your torrent status</p>`})}, (error) => {Swal.fire({icon: 'error',title: 'Oops...',text: "The API key you've inserted is invalid",footer: '<a href="">Why do I have this issue?</a>'})});
     return false;
 }
