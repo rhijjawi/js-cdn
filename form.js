87c6a4e686a63fea0491b02305ea3d5d69af4365
rhijjawi <18570300+rhijjawi@users.noreply.github.com>
2022-02-26 16:08:23 +0300

    Update form.js

--- form.js
+++ form.js
@@ -5,7 +5,7 @@ function subForm(){
     torname = document.getElementById('tname').value
     tor = document.getElementById('t').value
     req = {"magnet": tor, "name": torname}
-    const res = axios.post('/torrent/add-magnet', req, {headers: {'Authorization':'475ad896d774ce50ad01aa47fa5f3b7eaf4be2f148971f35'}
+    const res = axios.post('/torrent/add-magnet', req, {headers: {'Authorization' : document.getElementById('key').value}
     });
     console.log(res.data)
     return false;
