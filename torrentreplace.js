3caf4682a08ec3554dc4c9a2896eff161a340149
rhijjawi <18570300+rhijjawi@users.noreply.github.com>
2022-02-28 08:51:26 +0300

    Update torrentreplace.js

--- torrentreplace.js
+++ torrentreplace.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         let mag = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].querySelectorAll('a')[0];
         let name = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-name')[0].querySelectorAll('a')[0].innerHTML;
         mag = mag.href;
-        let urlenc = "https://api.ramzihijjawi.me/add-torrent?magnet="+mag
+        let urlenc = "https://api.mycloudtorrent.codes/add-torrent?magnet="+mag
         let source = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].getElementsByTagName('img')[1];
         if (typeof(source) == 'object') {
             let source = document.getElementById("torrents").getElementsByTagName("li")[i+1].getElementsByClassName('item-icons')[0].getElementsByTagName('img')[1].getAttribute('alt');
